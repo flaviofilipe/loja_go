c11312e8284c79c369507966a12a82c722a617d6
Flavio Filipe <flaviofilipe.dev@gmail.com>
2021-12-02 11:41:36 -0300

    create project

---
+++ database/produtos.sql
@@ -0,0 +1,7 @@
+create table produtos (
+    id serial primary key,
+    nome VARCHAR,
+    descricao VARCHAR,
+    preco DECIMAL,
+    quantidade INTEGER
+);
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/flaviofilipe/loja_go
+
+go 1.17
+
+require github.com/lib/pq v1.10.4
